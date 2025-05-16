160d4f4596d2f2e51363f9e35970d6f961e31881
mhz <batubattery25@gmail.com>
2025-05-16 21:52:36 +0700

    Refactor userController to remove error handling in login and register functions

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,31 +1,17 @@
 const {authenticateUser, createUser} = require("../service/authService");
 
 const login = async (req, res) => {
-    try {
         const {email, password} = req.body;
         const data = await authenticateUser(email, password);
         res.status(data.status).json(data);
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error',
-            error: error.message
-        });
-    }
 };
 const register = async (req, res) => {
-    try {
         const {name, email, password} = req.body;
         const data = await createUser(name, email, password);
         res.status(data.status).json({
             message: data.message,
             data: data.data
         });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error',
-            error: error.message
-        });
-    }
 }
 module.exports = {
     login,
